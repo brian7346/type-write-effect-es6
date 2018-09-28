b6416056e26d50dd152683888c17facd47d432d9
Max Roslow <doctorbrian97@gmail.com>
2018-09-28 16:53:30 +0400

    initial

---
+++ main.js
@@ -0,0 +1,69 @@
+//Constructor function - функция, вызванная с ключевым словом new возращает новый объект
+// Конструкторы не отличаются от обычных функций абсолютно ничем
+// Любая фунция в js  потенциально являются конструктором
+// Любую фунцию можно вызывать с ключевым словом new
+//ES6 class
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = "";
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    //Current index of word
+    const current = this.wordIndex % this.words.length;
+    //Get full text of current word
+    const fulltxt = this.words[current];
+
+    //Check if deleting
+    if (this.isDeleting) {
+      //Remove char
+      this.txt = fulltxt.substring(0, this.txt.length - 1); // ?????
+    } else {
+      //Add char
+      this.txt = fulltxt.substring(0, this.txt.length + 1); // ??????
+    }
+
+    //Insert txt into element
+    this.txtElement.innerHTML = `<span class='txt'>${this.txt}</span>`;
+    //Initial Type Speed
+    let typeSpeed = 300;
+    if (this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    //If word is complete
+    if (!this.isDeleting && this.txt === fulltxt) {
+      //Make Pause at end
+      typeSpeed = this.wait;
+      //Set delete to true
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      //Move to next word
+      this.wordIndex++;
+      //Pause before start typing
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+//Init On DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+//Init App
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  //Init TypeWriter
+  //Создали экземпляр класса TypeWriter(создали объект)
+  new TypeWriter(txtElement, words, wait);
+}
